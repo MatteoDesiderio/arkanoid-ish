affb0c844b98ede81423e70d84d1f59ce80f36de
MatteoDesiderio <4matteo.desiderio@gmail.com>
2026-08-04 10:25:21 +0100

    Add score label as variable to ui script

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var life_point_container: HBoxContainer = %"Life Point Container"
+@onready var score_label: RichTextLabel = %ScoreLabel
 
 func decrease_life_points_by_one() -> void:
 	for point : TextureRect in life_point_container.get_children():
