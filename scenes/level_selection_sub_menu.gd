9afbf4f4baedaf958bf4c6a30a08c834edd2675b
MatteoDesiderio <4matteo.desiderio@gmail.com>
2026-08-09 21:18:34 +0100

    Adjust size of the elements in grid

--- scenes/level_selection_sub_menu.gd
+++ scenes/level_selection_sub_menu.gd
@@ -1,21 +1,27 @@
 extends SubMenu
 
-@onready var levels_grid: GridContainer = %"Levels Grid"
+@onready var levels_grid: HFlowContainer = %"Levels Grid"
 
 func _ready() -> void:
 	super()
 
 
 func fill_grid(level_data_list : Array[LevelData]) -> void:
+	var n_columns : = 4
+	var width = _get_button_width(n_columns)
+	
 	for level_data : LevelData in level_data_list:
-		var button : = Button.new()
-		button.size_flags_vertical = Control.SIZE_EXPAND_FILL
-		button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		var panel : = Panel.new()
 		
-		button.text = "%s/%s\n" % [
-			level_data.level_index, 
-			level_data.number_of_levels
-			]
+		panel.custom_minimum_size.x = width
+		panel.custom_minimum_size.y = width
+		levels_grid.add_child(panel)
+		
 
-		levels_grid.add_child(button)
-		
+func _get_button_width(n_buttons_in_row : int) -> float:
+	var width : float = levels_grid.get_parent_area_size().x
+	var h_space : float = levels_grid.get_theme_constant("h_separation")
+	var button_width : float = (
+		(width - (n_buttons_in_row - 1) * h_space) / n_buttons_in_row
+		)
+	return button_width
